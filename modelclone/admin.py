661926055fa00e6526cdd20b6e2bbf3a32e8d866
Bruno Vila <bruno@iessanclemente.net>
2022-04-02 01:03:52 +0200

    change url to re_path

--- modelclone/admin.py
+++ modelclone/admin.py
@@ -5,7 +5,8 @@ try:
 except ImportError:
     # django < 1.7
     from django.contrib.admin.util import unquote
-from django.conf.urls import url
+#from django.conf.urls import url
+from django.urls import re_path as url
 from django.utils.encoding import force_text
 from django.utils.translation import ugettext as _
 from django.utils.translation import ugettext_lazy as lazy
